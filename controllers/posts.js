d2679f719041e3c7cd249b822403dca0e18c52ad
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-25 16:14:47 +0200

    Migliorato funzione show cercando dentro l'array e gestione post non trovato

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,7 +8,7 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    const id = Number (req.params.id);
+    const id = Number(req.params.id);
 
     if (isNaN(id) || id <= 0) {
         res.status(400).json({
@@ -18,6 +18,22 @@ function show(req, res) {
 
         return;
     }
+
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        res.status(404).json({
+            error: `Post con id ${id} non trovato`,
+            item: null,
+        });
+
+        return;
+    }
+
+    res.status(200).json({
+        item: post,
+    });
 }
+
 
 export { index, show, create, update, destroy };

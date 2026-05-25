5e9a4fcbac8b3789f1a8a6af58b774ac477b4f60
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-25 15:28:01 +0200

    Importato array dei post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,9 @@
 import posts from "../data/posts.js";
 
-
+function index(req, res) {
+    res.json({
+        message: "Lista dei post",
+    });
+}
 
 export { index, show, create, update, destroy };

c21b009b8532661ed80db94c1835bb83cb6adf8a
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-25 15:46:08 +0200

    Aggiunta funzione show

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,4 +7,17 @@ function index(req, res) {
     });
 }
 
+function show(req, res) {
+    const id = Number (req.params.id);
+
+    if (isNaN(id) || id <= 0) {
+        res.status(400).json({
+            error: "L'id deve esesere un numero positivo",
+            item: null,
+        });
+
+        return;
+    }
+}
+
 export { index, show, create, update, destroy };

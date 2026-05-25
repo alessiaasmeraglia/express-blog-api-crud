c9a6c8cf04af477c449aa9a6db31fb4a3a8df109
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-25 16:46:09 +0200

    Aggiunta funzione create e update

--- controllers/posts.js
+++ controllers/posts.js
@@ -35,5 +35,42 @@ function show(req, res) {
     });
 }
 
+function create(req, res) {
+    const newPostData = req.body;
+
+    console.log("Dati ricevuti:", newPostData);
+
+    res.status(201).json({
+        message: "Stai provando a creare dei dati",
+        dati: newPostData,
+    });
+}
+
+function update(req, res) {
+    const id = Number(req.params.id);
+
+    if (isNaN(id) || id <= 0) {
+        res.status(400).json({
+            error: "L'id deve essere un numero positivo",
+        });
+
+        return;
+    }
+
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        res.status(404).json({
+            error: `Post con id ${id} non trovato`,
+        });
+
+        return;
+    }
+
+    res.status(200).json({
+        message: `Modifica del post ${id}`,
+    });
+}
+
 
 export { index, show, create, update, destroy };
